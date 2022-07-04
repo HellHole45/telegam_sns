f1216a70747f444b4a403baa6417fd7814db74e6
Amir Akhmetov <103672939+HellHole45@users.noreply.github.com>
2022-07-04 14:51:14 +0300

    Update sns.py

--- sns.py
+++ sns.py
@@ -17,14 +17,18 @@ menu_3 = KeyboardButton(text='Тех. Поддержка', callback_data="menu_3
 keyboard.add(menu_1, menu_2, menu_3)
 
 keyboard2 = InlineKeyboardMarkup()
-menu_1 = InlineKeyboardButton(text='Corvus', callback_data="menu_1")
-menu_2 = InlineKeyboardButton(text='MAD', callback_data="menu_2")
-menu_3 = InlineKeyboardButton(text='Nictech', callback_data="menu_3")
-keyboard2.add(menu_1, menu_2, menu_3)
+menu_1 = InlineKeyboardButton(text='FAFF (R and M)', callback_data="menu_1")
+menu_2 = InlineKeyboardButton(text='AQRA', callback_data="menu_2")
+menu_3 = InlineKeyboardButton(text='KASTA', callback_data="menu_3")
+menu_1 = InlineKeyboardButton(text='AK247', callback_data="menu_4")
+menu_2 = InlineKeyboardButton(text='CHN', callback_data="menu_5")
+menu_3 = InlineKeyboardButton(text='RED', callback_data="menu_6")
+
+keyboard2.add(menu_1, menu_2, menu_3, menu_4, menu_5, menu_6)
 
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
-    await bot.send_message(message.from_user.id, 'Привет! Это бот продажи снюса.\nПочему именно мы? У нас полностью автоматический магазин, после оплаты вам сразу отпишет оператор, после чего вы сможете успешно оформить доставку.', reply_markup=keyboard)
+    await bot.send_message(message.from_user.id, 'Привет! Это бот продажи табка_рус.\nПочему именно мы? У нас полностью автоматический магазин, после оплаты вам сразу отпишет оператор, после чего вы сможете успешно оформить доставку,\n После оплаты вам скинут sms трекер', reply_markup=keyboard)
 
 @dp.callback_query_handler(text_contains='menu_')
 async def menu(call: types.CallbackQuery):
@@ -33,11 +37,17 @@ async def menu(call: types.CallbackQuery):
         if code.isdigit():
             code = int(code)
         if code == 1:
-            await call.message.edit_text(f'Corvus Fenix\n\nЦена: 250 рублей\n\nОплатить товар можно на номер {number} (QIWI)\nПосле оплаты вам сразу отпишет оператор', reply_markup=keyboard2)
+            await call.message.edit_text(f'FAFF (R and M)\n\nЦена: 880 рублей\n\nОплатить товар можно на номер {number} (QIWI)\nПосле оплаты вам сразу, отпишите админу + скрин чека', reply_markup=keyboard2)
         if code == 2:
-            await call.message.edit_text(f'MAD fatality\n\nЦена: 250 рублей\n\nОплатить товар можно на номер {number} (QIWI)\nПосле оплаты вам сразу отпишет оператор', reply_markup=keyboard2)
+            await call.message.edit_text(f'AQRA\n\nЦена: 450 рублей\n\nОплатить товар можно на номер {number} (QIWI)\nПосле оплаты вам сразу, отпишите админу + скрин чека', reply_markup=keyboard2)
         if code == 3:
-            await call.message.edit_text(f'Nictech Strong\n\nЦена: 250 рублей\n\nОплатить товар можно на номер {number} (QIWI)\nПосле оплаты вам сразу отпишет оператор', reply_markup=keyboard2)
+            await call.message.edit_text(f'KASTA\n\nЦена: 700 рублей\n\nОплатить товар можно на номер {number} (QIWI)\nПосле оплаты вам сразу, отпишите админу + скрин чека', reply_markup=keyboard2)
+        if code == 4:
+            await call.message.edit_text(f'AK247\n\nЦена: 899 рублей\n\nОплатить товар можно на номер {number} (QIWI)\nПосле оплаты вам сразу, отпишите админу + скрин чека', reply_markup=keyboard2)
+        if code == 5:
+            await call.message.edit_text(f'CHN\n\nЦена: 450 рублей\n\nОплатить товар можно на номер {number} (QIWI)\nПосле оплаты вам сразу, отпишите админу + скрин чека', reply_markup=keyboard2)
+        if code == 6:
+            await call.message.edit_text(f'RED\n\nЦена: 550 рублей\n\nОплатить товар можно на номер {number} (QIWI)\nПосле оплаты вам сразу, отпишите админу + скрин чека', reply_markup=keyboard2)
 
 @dp.message_handler(content_types=["text"])
 async def do_something(message: types.Message):
@@ -52,4 +62,3 @@ async def do_something(message: types.Message):
 
 
 executor.start_polling(dp)
-
